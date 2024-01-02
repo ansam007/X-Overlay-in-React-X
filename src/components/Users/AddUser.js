3410d412a47e00b1a8c7d2aea6620f304921b970
ansam007 <anshul.sharma5271@gmail.com>
2024-01-02 13:41:05 +0530

    add error messages and render that on screen

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,29 @@
 import Card from '../UI/Card';
 import classes from './AddUser.module.css'
 import {useState} from 'react';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
     const[enterUserName, setEnterUserName] = useState('');
     const[enterAge, setEnterAge] = useState('');
+    const[error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
 
         if(enterUserName.trim().length === 0 || enterAge.trim().length === 0){
-            return;
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid name and age (non-empty values)'
+            })
+            return;  
         }
 
         if(enterAge < 1){
+            setError({
+                title:'Invalid age',
+                message: 'Please enter valid age'   
+            })
             return;
         }
 
@@ -32,7 +42,13 @@ const AddUser = (props) => {
         setEnterAge(event.target.value);
     }
 
+    const errorHandler = () => {
+        setError(null);
+    }
+
   return (
+    <div>
+    {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}></ErrorModal>}
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">UserName</label>
@@ -42,6 +58,7 @@ const AddUser = (props) => {
         <button type="submit">Add User</button>
       </form>
     </Card>
+    </div>
   );
 };
 
