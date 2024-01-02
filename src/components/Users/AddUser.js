55e577f2374bf3d763fa5d5ee8a0a9d6dbb96086
ansam007 <anshul.sharma5271@gmail.com>
2024-01-02 11:49:58 +0530

    add validations like if age is invalid or user doesn't enter anything then it checks that cases

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,17 +1,41 @@
 import Card from '../UI/Card';
 import classes from './AddUser.module.css'
+import {useState} from 'react';
 
 const AddUser = (props) => {
-  const addUserHandler = (event) => {
-    event.preventDefault();
-  };
+    const[enterUserName, setEnterUserName] = useState('');
+    const[enterAge, setEnterAge] = useState('');
+
+    const addUserHandler = (event) => {
+        event.preventDefault();
+
+        if(enterUserName.trim().length === 0 || enterAge.trim().length === 0){
+            return;
+        }
+        
+        if(enterAge < 1){
+            return;
+        }
+
+        setEnterUserName('');
+        setEnterAge('');
+    }
+
+    const userNameChangeHandler = (event) => {
+        setEnterUserName(event.target.value);
+    }
+
+    const ageChangeHandler = (event) => {
+        setEnterAge(event.target.value);
+    }
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">UserName</label>
-        <input id="username" type="text"></input>
+        <input id="username" value={enterUserName} type="text" onChange={userNameChangeHandler}></input>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"></input>
+        <input id="age" value={enterAge} type="number" onChange={ageChangeHandler}></input>
         <button type="submit">Add User</button>
       </form>
     </Card>
